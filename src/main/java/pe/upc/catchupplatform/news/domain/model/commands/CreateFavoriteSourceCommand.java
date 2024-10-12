d1ebba50303779241b8b7502ca4150f87c71be29
Ricardo <richiabv@gmail.com>
2024-10-11 21:23:18 -0500

    Initial commit

---
+++ src/main/java/pe/upc/catchupplatform/news/domain/model/commands/CreateFavoriteSourceCommand.java
@@ -0,0 +1,10 @@
+package pe.upc.catchupplatform.news.domain.model.commands;
+
+public record CreateFavoriteSourceCommand(String newsApiKey, String sourceId) {
+    public CreateFavoriteSourceCommand {
+        if (newsApiKey == null || newsApiKey.isEmpty())
+            throw new IllegalArgumentException("NewsApiKey cannot be null or empty");
+        if (sourceId == null || sourceId.isEmpty())
+            throw new IllegalArgumentException("SourceId cannot be null or empty");
+    }
+}
